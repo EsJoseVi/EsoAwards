bf7950967699c588a8ae01b635de861691523d7e
JoseVi <rmcomando3@gmail.com>
2024-06-13 22:22:54 +0200

    added auto updating

---
+++ update.bash
@@ -0,0 +1,5 @@
+npm run build
+docker build -t rmcomando34/esoawardsapp .
+docker tag rmcomando34/esoawardsapp rmcomando34/esoawardsapp:v3.0
+docker push rmcomando34/esoawardsapp:v3.0
+ssh root@172.233.115.41 'docker pull rmcomando34/esoawardsapp:v3.0;docker stop esoawards;docker rm esoawards;docker run -p 80:80 --name esoawards rmcomando34/esoawardsapp:v3.0'
